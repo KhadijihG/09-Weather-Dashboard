8d3b1fd2481c60404d1c1e6ea79409ef92ed01d9
KhadijihG <dbaygore9@gmail.com>
2024-12-05 16:35:50 -0500

    added historyService

--- server/src/routes/api/weatherRoutes.ts
+++ server/src/routes/api/weatherRoutes.ts
@@ -1,7 +1,7 @@
 import { Router } from 'express';
 const router = Router();
 
-// import HistoryService from '../../service/historyService.js';
+import HistoryService from '../../service/historyService.js';
 import WeatherService from '../../service/weatherService.js';
 
 // TODO: POST Request with city name to retrieve weather data
@@ -9,13 +9,18 @@ router.post('/', async(req, res) => {
   console.log(req.body);
   const weatherService = new WeatherService ('','',req.body.cityName)
  const cityWeather = await weatherService.getWeatherForCity(req.body.cityName)
+ await HistoryService.addCity(req.body.cityName)
  res.json(cityWeather)
   // TODO: GET weather data from city name
   // TODO: save city to search history
 });
 
 // TODO: GET search history
-router.get('/history', async (_req, _res) => {});
+router.get('/history', async (_req, res) => {
+ const history = await HistoryService.read() 
+ console.log(history);
+ res.json(history)
+});
 
 // * BONUS TODO: DELETE city from search history
 router.delete('/history/:id', async (_req, _res) => {});
--- server/src/service/historyService.ts
+++ server/src/service/historyService.ts
@@ -6,12 +6,12 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 // TODO: Define a City class with name and id properties
 class City {
-  name:string;
-  id:string;
+  name: string;
+  id: string;
   constructor(
-    name:string,
-    id:string
-  ){
+    name: string,
+    id?: string
+  ) {
     this.name = name;
     this.id = id || uuid()
   }
@@ -22,16 +22,27 @@ class HistoryService {
   pathToDb = path.join(__dirname, "../../db/searchHistory.json")
   // TODO: Define a read method that reads from the searchHistory.json file
   async read() {
-    const cities = fs.readFileSync(this.pathToDb)
-    console.log(cities);
-    
+    const cities = fs.readFileSync(this.pathToDb, "utf-8")
+    const parseCities = JSON.parse(cities).map((city: any) => new City(city.name, city.id))
+    console.log(parseCities);
+    return parseCities
   }
   // TODO: Define a write method that writes the updated cities array to the searchHistory.json file
   // private async write(cities: City[]) {}
   // TODO: Define a getCities method that reads the cities from the searchHistory.json file and returns them as an array of City objects
   // async getCities() {}
   // TODO Define an addCity method that adds a city to the searchHistory.json file
-  async addCity(city: string) {}
+  async addCity(city: string) {
+    // Read from the database
+    const cities = await this.read()
+    //Check if the new city already exist
+    const cityNames = cities.map((element: any) => element.name)
+    if (cityNames.includes(city)) return
+    //Push the new city to the cities array
+    cities.push(new City(city))
+    //Write to the file
+    fs.writeFileSync(this.pathToDb, JSON.stringify(cities))
+  }
   // * BONUS TODO: Define a removeCity method that removes a city from the searchHistory.json file
   // async removeCity(id: string) {}
 }
